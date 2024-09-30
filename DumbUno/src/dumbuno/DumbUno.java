4b8807b3c165a0c5cd2c6e854bf60d9425b6ad9d
James Vanderhyde <vanderhyde@sxu.edu>
2024-09-30 10:14:09 -0500

    Created NetBeans project

---
+++ DumbUno/src/dumbuno/DumbUno.java
@@ -0,0 +1,12 @@
+//This is an Uno game simulation.
+//Created by James Vanderhyde, 30 September 2024
+
+package dumbuno;
+
+public class DumbUno
+{
+    public static void main(String[] args) 
+    {
+    }
+    
+}
